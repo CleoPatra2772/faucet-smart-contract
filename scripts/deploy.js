c01fae28d6b98029a12c1c589805a2bf1e24abb9
Cleo Gao <celeo127@outlook.com>
2022-10-11 14:19:55 +0800

    deployed token on goerli

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,31 +1,35 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
+
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const DragonfruitToken = await hre.ethers.getContractFactory("DragonFruitToken");
+  const dragonfruitToken = await DragonfruitToken.deploy(100000000, 50);
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  await dragonfruitToken.deployed();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log("Dragonfruit Token deployed: ", dragonfruitToken.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+
+// const { ethers } = require("hardhat");
+// const hre = require("hardhat");
+
+// async function main() {
+ 
+//   const DragonfruitToken = await ethers.getContractFactory('DragonFruitToken');
+//   const dragonfruitToken = DragonfruitToken.deploy(100000000, 50);
+
+//   //await dragonfruitToken.deployed();
+
+//   console.log("Dragonfruit Token deployed at: ",  dragonfruitToken.address);
+// }
+
+// main().catch((error) => {
+//   console.error(error);
+//   process.exitCode = 1;
+// });
